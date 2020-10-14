2971467f93b5a72298129426ad9a9ee14977a136
andinet Gebre <andinetmn@gmail.com>
2020-10-14 13:27:52 -0500

    second commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,48 +2,51 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        int num1,num2,res;
-        Scanner input=new Scanner(System.in);
+        int num1, num2, res;
+        Scanner input = new Scanner(System.in);
         System.out.print("Welcome to the Dream Corp Calculator! Type two numbers.");
-        num1=input.nextInt();
-        num2=input.nextInt();
+        num1 = input.nextInt();
+        num2 = input.nextInt();
         System.out.print(" Now pick an operand!");
 
         char operator = input.next().charAt(0);
 
 
-        if(operator=='+'){
-            res =num1+num2;
+        if (operator == '+') {
+            res = num1 + num2;
+            System.out.println(res);
+
+
+        } else if (operator == '*') {
+            res = num1 * num2;
+            System.out.println(res);
+
+
+        } else if (operator == '-') {
+            res = num1 - num2;
             System.out.println(res);
 
 
         }
 
-        else if(operator=='*'){
-            res =num1*num2;
-            System.out.println(res);
+        else if (operator == '/') {
+         try{
+            res = num1 / num2;
+            System.out.println(res);}
+
+            catch (ArithmeticException e) {
+                System.out.println("Exception caught: Division by zero is not possible.Try non Zero Denominator");
+            }
 
 
-        }
-        else if(operator=='-'){
-            res =num1-num2;
-            System.out.println(res);
-
-
-        }
-        else if(operator=='/'){
-            res =num1/num2;
-            System.out.println(res);
-
-
-        }
-
-
-        else{
+        } else {
             System.out.println("You input wrong operator!Please try again!!");
 
 
+        }
+
+
+}
 
 
     }
-}}
