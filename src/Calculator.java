738b57ff3bb515d4ab071b5bd36b8079ac6689bc
andinet Gebre <andinetmn@gmail.com>
2020-10-14 11:58:54 -0500

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int num1,num2,res;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Welcome to the Dream Corp Calculator! Type two numbers.");
+        num1=input.nextInt();
+        num2=input.nextInt();
+        System.out.print(" Now pick an operand!");
+
+        char operator = input.next().charAt(0);
+
+
+        if(operator=='+'){
+            res =num1+num2;
+            System.out.println(res);
+
+
+        }
+
+        else if(operator=='*'){
+            res =num1*num2;
+            System.out.println(res);
+
+
+        }
+        else if(operator=='-'){
+            res =num1-num2;
+            System.out.println(res);
+
+
+        }
+        else if(operator=='/'){
+            res =num1/num2;
+            System.out.println(res);
+
+
+        }
+
+
+        else{
+            System.out.println("You input wrong operator!Please try again!!");
+
+
+
+
+    }
+}}
